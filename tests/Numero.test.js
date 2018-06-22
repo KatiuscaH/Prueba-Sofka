c3f0221ac586bce2c0bc0954533faf1152d39e3e
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-06-22 15:43:34 -0500

    Agregando test individuales a los numeros

--- tests/Numero.test.js
+++ tests/Numero.test.js
@@ -1,4 +1,14 @@
-const { numeros, numeroCero } = require('../src/Numero');
+const { numeros, numeroCero,
+    numeroUno,
+    numeroDos,
+    numeroTres,
+    numeroCuatro,
+    numeroCinco,
+    numeroSeis,
+    numeroSiete,
+    numeroOcho,
+    numeroNueve
+    } = require('../src/Numero');
 
 describe('Numeros', () => {
     it('Retorna el numero de COLUMNAS de la matriz', () => {
@@ -25,8 +35,125 @@ describe('Numeros impresos', () => {
             ["|", " ", "|"],
             [" ", "-", " "]
         ]
-        expect(numeroCero(1)).toEqual(matrix)
+        expect(n).toEqual(matrix)
 
     })
 
+    it('Retorna el numero UNO impreso con tamaño 1', () => {
+        const n = numeroUno(1);
+        const matrix = [
+            [" ", " ", " "],
+            [" ", " ", "|"],
+            [" ", " ", " "],
+            [" ", " ", "|"],
+            [" ", " ", " "]
+        ]
+        expect(numeroUno(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero DOS impreso con tamaño 1', () => {
+        const n = numeroDos(1);
+        const matrix = [
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", "-", " "],
+            ["|", " ", " "],
+            [" ", "-", " "]
+        ]
+        expect(numeroDos(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero TRES impreso con tamaño 1', () => {
+        const n = numeroTres(1);
+        const matrix = [
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", "-", " "]
+        ]
+        expect(numeroTres(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero CUATRO impreso con tamaño 1', () => {
+        const n = numeroCuatro(1);
+        const matrix = [
+            [" ", " ", " "],
+            ["|", " ", "|"],
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", " ", " "]
+        ]
+        expect(numeroCuatro(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero CINCO impreso con tamaño 1', () => {
+        const n = numeroCinco(1);
+        const matrix = [
+            [" ", "-", " "],
+            ["|", " ", " "],
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", "-", " "]
+        ]
+        expect(numeroCinco(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero SEIS impreso con tamaño 1', () => {
+        const n = numeroSeis(1);
+        const matrix = [
+            [" ", "-", " "],
+            ["|", " ", " "],
+            [" ", "-", " "],
+            ["|", " ", "|"],
+            [" ", "-", " "]
+        ]
+        expect(numeroSeis(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero SIETE impreso con tamaño 1', () => {
+        const n = numeroSiete(1);
+        const matrix = [
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", " ", " "],
+            [" ", " ", "|"],
+            [" ", " ", " "]
+        ]
+        expect(numeroSiete(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero OCHO impreso con tamaño 1', () => {
+        const n = numeroOcho(1);
+        const matrix = [
+            [" ", "-", " "],
+            ["|", " ", "|"],
+            [" ", "-", " "],
+            ["|", " ", "|"],
+            [" ", "-", " "]
+        ]
+        expect(numeroOcho(1)).toEqual(matrix)
+
+    })
+
+    it('Retorna el numero NUEVE impreso con tamaño 1', () => {
+        const n = numeroNueve(1);
+        const matrix = [
+            [" ", "-", " "],
+            ["|", " ", "|"],
+            [" ", "-", " "],
+            [" ", " ", "|"],
+            [" ", "-", " "]
+        ]
+        expect(numeroNueve(1)).toEqual(matrix)
+
+    })
 });
+
